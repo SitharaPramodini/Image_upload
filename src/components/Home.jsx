6d549b9e1ccf25dad9ea84624c6cfe7cacf42b60
SitharaPramodini <it21306754@my.sliit.lk>
2025-02-10 16:40:17 +0530

    camera

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,73 +1,76 @@
-import React, { useState, useRef } from "react";
+import React, { useRef, useState } from "react";
+import Header from "./Header";
+import Webcam from "react-webcam";
 
 function Home() {
-    const [stream, setStream] = useState(null);
-    const [image, setImage] = useState(null);
-    const videoRef = useRef(null);
-    const canvasRef = useRef(null);
-
-    const openCamera = async () => {
-        try {
-            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-            setStream(stream);
-            if (videoRef.current) {
-                videoRef.current.srcObject = stream;
-            }
-        } catch (error) {
-            console.error("Error accessing camera:", error);
-        }
+    const webcamRef = useRef(null);
+    const [capturedImage, setCapturedImage] = useState(null);
+    
+    const capturePhoto = () => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setCapturedImage(imageSrc);
     };
 
-    const captureImage = () => {
-        if (videoRef.current && canvasRef.current) {
-            const context = canvasRef.current.getContext("2d");
-            context.drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height);
-            const imageData = canvasRef.current.toDataURL("image/png");
-            setImage(imageData);
-            stopCamera();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        
+        if (capturedImage) {
+            formData.append("photo", capturedImage);
         }
-    };
 
-    const stopCamera = () => {
-        if (stream) {
-            stream.getTracks().forEach(track => track.stop());
-            setStream(null);
-        }
+        // Handle form submission, e.g., send to server
+        console.log("Form submitted", Object.fromEntries(formData));
     };
 
     return (
-        <div className="container max-w-full pt-3 pb-14 bg-[#f4f8fb] w-full absolute rounded-t-3xl h-auto">
-            <form className="mx-8 my-8">
-                {/* Other input fields */}
-
-                <div className="relative z-0 w-full mb-5 group">
-                    <button type="button" onClick={openCamera} className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5">
-                        Take Your Picture
-                    </button>
-                </div>
-
-                {stream && (
-                    <div className="relative">
-                        <video ref={videoRef} autoPlay className="w-full h-auto" />
-                        <button type="button" onClick={captureImage} className="mt-2 text-white bg-green-600 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5">
-                            Capture Image
-                        </button>
+        <div>
+            <Header />
+            <Webcam ref={webcamRef} screenshotFormat="image/jpeg" className="w-full h-auto" />
+            <div className="container mt-[8rem] max-w-full pt-3 pb-14 bg-[#f4f8fb] w-full absolute rounded-t-3xl h-auto">
+                <form className="mx-8 pt-6" onSubmit={handleSubmit}>
+                    <div className="relative z-0 w-full mb-5 group">
+                        <input type="email" name="email" className="block py-2.5 px-0 w-full text-sm bg-transparent border-b-2 border-gray-300 focus:border-red-600" placeholder=" " required />
+                        <label className="absolute text-sm text-gray-500 top-3">Email address</label>
                     </div>
-                )}
-
-                {image && (
-                    <div className="mt-4">
-                        <img src={image} alt="Captured" className="w-full h-auto" />
-                        <input type="hidden" name="captured_image" value={image} />
+                    
+                    <div class="relative z-0 w-full mb-5 group">
+                        <input type="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-300 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-600 peer" placeholder=" " required />
+                        <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-red-600 peer-focus:dark:text-red-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                     </div>
-                )}
-
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5">
-                    Submit
-                </button>
-            </form>
-
-            <canvas ref={canvasRef} style={{ display: "none" }} width={640} height={480} />
+                    <div class="relative z-0 w-full mb-5 group">
+                        <input type="password" name="repeat_password" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-300 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-600 peer" placeholder=" " required />
+                        <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-red-600 peer-focus:dark:text-red-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
+                    </div>
+                    <div class="grid md:grid-cols-2 md:gap-6">
+                        <div class="relative z-0 w-full mb-5 group">
+                            <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-300 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-600 peer" placeholder=" " required />
+                            <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-red-600 peer-focus:dark:text-red-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+                        </div>
+                        <div class="relative z-0 w-full mb-5 group">
+                            <input type="text" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-300 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-600 peer" placeholder=" " required />
+                            <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-red-600 peer-focus:dark:text-red-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+                        </div>
+                    </div>
+                    <div class="grid md:grid-cols-2 md:gap-6">
+                        <div class="relative z-0 w-full mb-5 group">
+                            <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-300 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-600 peer" placeholder=" " required />
+                            <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-red-600 peer-focus:dark:text-red-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (123-456-7890)</label>
+                        </div>
+                        <div class="relative z-0 w-full mb-5 group">
+                            <input type="text" name="floating_company" id="floating_company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-300 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-600 peer" placeholder=" " required />
+                            <label for="floating_company" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-red-600 peer-focus:dark:text-red-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
+                        </div>
+                    </div>
+                    <div className="relative z-0 w-full mb-5 group">
+                        <button type="button" onClick={capturePhoto} className="text-white bg-blue-700 hover:bg-blue-800 px-5 py-2.5 rounded-lg">Capture Photo</button>
+                    </div>
+                    
+                    {capturedImage && <img src={capturedImage} alt="Captured" className="w-40 h-40 mt-4" />}
+                    
+                    <button type="submit" className="text-white bg-red-700 hover:bg-red-800 px-5 py-2.5 rounded-lg">Submit</button>
+                </form>
+            </div>
         </div>
     );
 }
